c027ece0e3bc5ef4df978feed714fe11b01cffb1
codjoelmayer <tmjoelm4@gmail.com>
2023-01-03 15:03:17 +0200

    Deploy to firebase

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Home</h2>
   </div>
 </template>
 
@@ -12,7 +11,6 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
